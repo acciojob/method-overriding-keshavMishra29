de4b5dcbb8764c60ed3bfa4ce6521f095741bfed
keshav <keshavkumar584@gmail.com>
2024-07-16 20:32:51 +0530

    Assignment Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        A keshav = new A();
+        System.out.println(keshav.meth());
         B obj = new B();
         System.out.println(obj.meth());
     }
